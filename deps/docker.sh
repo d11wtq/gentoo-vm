98632897a84238d393e46bbf6de764afa2f9722f
d11wtq <chris@w3style.co.uk>
2014-02-14 01:56:52 +1100

    Not currently working, but fairly close (needs a kernel config change)

--- deps/docker.sh
+++ deps/docker.sh
@@ -1,9 +1,44 @@
 docker() {
-  is_met() {
-    which docker
+  installed() {
+    is_met() {
+      which docker 2>/dev/null
+    }
+
+    meet() {
+      pkg_name=app-emulation/docker-0.8.0
+      sudo emerge =$pkg_name --autounmask-write
+      sudo etc-update --automode -3
+      sudo emerge =$pkg_name
+    }
+
+    process
   }
 
-  meet() {
-    sudo emerge -av app-emulation/docker
+  rc() {
+    is_met() {
+      sudo rc-status default | grep docker
+    }
+
+    meet() {
+      sudo rc-update add docker default
+    }
+
+    process
   }
+
+  running() {
+    is_met() {
+      /etc/init.d/docker status | grep started
+    }
+
+    meet() {
+      sudo /etc/init.d/docker start
+    }
+
+    process
+  }
+
+  requires installed
+  requires rc
+  requires running
 }
--- deps/vm.sh
+++ deps/vm.sh
@@ -1,5 +1,3 @@
 vm() {
-  requires "docker"
-
-  process
+  requires docker
 }
