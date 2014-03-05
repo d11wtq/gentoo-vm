d947537bf509b90a19bc279d4ab0127486a49273
d11wtq <chris@w3style.co.uk>
2014-03-05 07:00:12 +0000

    Set up proper git aliases etc

--- deps/git.sh
+++ deps/git.sh
@@ -1,7 +1,16 @@
 git() {
+  libexec_path() {
+    re='s/.* in \x27([^ ]+)\x27.*/\1/'
+    git help -a | grep "git commands in" | sed -r "$re"
+  }
+
+  has_https() {
+    ls "$(libexec_path)/git-remote-https" >/dev/null 2>&1
+  }
+
   installed() {
     is_met() {
-      which git && [[ -f /usr/libexec/git-core/git-remote-https ]]
+      which git && has_https
     }
 
     meet() {
@@ -9,18 +18,42 @@ git() {
     }
   }
 
-  configured() {
+  config() {
+    key=$1
+    val=$2
+
     is_met() {
-      [[ `git config user.name` = "d11wtq" ]] &&
-        [[ `git config user.email` = "chris@w3style.co.uk" ]]
+      [[ `git config "$key"` = "$val" ]]
     }
 
     meet() {
-      git config --global user.name  d11wtq
-      git config --global user.email chris@w3style.co.uk
+      git config --global "$key" "$val"
     }
+  }
+
+  configured() {
+    require config user.name  d11wtq
+    require config user.email chris@w3style.co.uk
+    require config color.ui   true
+    require config alias.st   status
+    require config alias.co   checkout
+  }
+
+  custom_commands() {
+    git_ff() {
+      is_met() {
+        which git-ff
+      }
+
+      meet() {
+        sudo cp ./deps/git/git-ff /usr/local/bin
+      }
+    }
+
+    require git_ff
   }
 
   require installed
   require configured
+  require custom_commands
 }
