633115b9193fe15e927d6e3a86c6fbd1816f1be7
d11wtq <chris@w3style.co.uk>
2014-06-06 16:12:55 +1000

    Check for specific version of fig

--- deps/fig.sh
+++ deps/fig.sh
@@ -1,10 +1,12 @@
 fig() {
+  pkg_ver=0.4.1
+  pkg_url=https://github.com/orchardup/fig/releases/download/$pkg_ver/linux
+
   is_met() {
-    which fig
+    which fig && fig --version | grep $pkg_ver
   }
 
   meet() {
-    pkg_url=https://github.com/orchardup/fig/releases/download/0.4.1/linux
     sudo sh -c "curl -sL $pkg_url > /usr/local/bin/fig"
     sudo chmod +x /usr/local/bin/fig
   }
